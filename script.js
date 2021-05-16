b4b405bd6f5a7b38e1f30f1d5d60bb2e676d3a41
lillie <lillie@Lillies-MBP.lan>
2021-05-15 23:50:51 -0700

    added var

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqurstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ"
+var numeric = 1234567890
+var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+console.log(lowerCase);
+console.log(upperCase);
+console.log(numeric);
+console.log(specialCharacter);
+
+
+
 
 // Write password to the #password input
 function writePassword() {
