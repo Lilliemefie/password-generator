3fe7630530818e87466ba6d27ac8a2da4db55cba
lillie <lillie@Lillies-MBP.lan>
2021-05-16 23:27:06 -0700

    added promt and confirm window for length and characters

--- script.js
+++ script.js
@@ -6,27 +6,41 @@ var numeric = "0123456789";
 var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
-// 1. create pop up for asking about:
-//    - How many character do you want? (from 8 to 128)
-//    - Do you want uppercase?
-//    - Do you want lowercase?
-//    - Do you want number?
-//    - Do you want symbols?
-// 2. 
-
-
-
 // Write password to the #password input, created function
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+ // var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ // passwordText.value = password;
 
+//};
+
+
+// Prompt to confirm how many characters the user would like to have in the password
+var lengthNo = prompt("How many characters would you like? Choose between 8 and 128");
+
+if (lengthNo < 8 || lengthNo > 128){
+prompt ("Please choose minimum 8 characters or maximum 128 characters!");
+};
+
+//confirm window which characters the user would like to use
+var confirmLower = confirm("Would you like to include lowercase leters?");
+var confirmUpper = confirm("Would you like to include uppercase letters?");
+var confirmNumeric = confirm("Would you like to include number?");
+var confirmSymbols = confirm("Would you like to include special characters?");
+// Loop if answer is outside the parameters 
+while(confirmUpper === false && confirmLower === false && confirmSymbols === false && confirmNumeric === false) {
+    alert("You must choose at least one character");
+    var confirmLower = confirm("Would you like to include lowercase leters?");
+    var confirmUpper = confirm("Would you like to include uppercase letters?");
+    var confirmNumeric = confirm("Would you like to include number?");
+    var confirmSymbols = confirm("Would you like to include special characters?");  
 }
 
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 
