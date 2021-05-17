520ee78bd2c153884a720de3e3678b2111b4c61f
lillie <lillie@Lillies-MBP.lan>
2021-05-16 23:12:41 -0700

    added var

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password")
+var lowerCase = "abcdefghijklmnopqurstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
+var numeric = "0123456789";
+var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 
 // 1. create pop up for asking about:
 //    - How many character do you want? (from 8 to 128)
@@ -8,14 +13,8 @@
 //    - Do you want number?
 //    - Do you want symbols?
 // 2. 
-//
-/*var randomFunc = {
-  lower = getRandomlowerCase,
-  upper = getRandomUpperCase,
-  symbols = getRandomSymbols,
-  number = getRandomNumeric,
 
-}*/
+
 
 // Write password to the #password input, created function
 function writePassword() {
@@ -25,44 +24,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-function getRandomSymbols() {
-  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-function getRandomlowerCase() {
-  var lowerCase = "abcdefghijklmnopqurstuvwxyz";
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-
-function getRandomUpperCase() {
-  var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-
-function getRandomNumeric() {
-  var numeric = "0123456789";
-  return numeric[Math.floor(Math.random() * numeric.length)];
-}
-
-
-function asklower(){
-  var yes = confirm ("Do you want a lowercase?");
-  if (yes === true);
-  getRandomlowerCase;
-} else {
-}
-});
-
-
-function askupper(){
-  var yes = confirm ("Do you want an uppercase?");
-  if (yes === true);
-  return getRandomUpperCase;
-}
-
-console.log(asklower, askupper);
 
 
 // Add event listener to generate button
