1f312cc23200a9068048962cf1f34ae54bb5cb66
lillie <lillie@Lillies-MBP.lan>
2021-05-17 02:40:59 -0700

    updated var

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password")
-var lowerCase = "abcdefghijklmnopqurstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
-var numeric = "0123456789";
-var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var pickedChar = "";
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "#", "$", "%", "&", "+", "-", ".", "/", "<", "=", ">", "?", "@"];
+var result = "";
+
 
 // Write password to the #password input, created function
 function writePassword() {
@@ -12,7 +13,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-};
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -39,12 +40,31 @@ if(confirmUpper === false && confirmLower === false && confirmSymbols === false 
     return generatePassword()
 }
 
+  
+
+var selectedChar = [];
+
+if(confirmLower === true){
+  (selectedChar.push(lowerCase));
+}
+
+if(confirmUpper === true){
+  (selectedChar.push(upperCase));
+}
+
+if(confirmNumeric === true){
+  (selectedChar.push(numeric));
+}
+
+if(confirmSymbols === true){
+  (selectedChar.push(symbols);
+}
+
+
+
+
 
 
 
 
 };
-
-
-
-
