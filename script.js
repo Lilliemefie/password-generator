4ff5e6838dfd1a4ab61ed770d09e8a126e84d1d1
lillie <lillie@Lillies-MBP.lan>
2021-05-16 23:57:59 -0700

    updated prompt window

--- script.js
+++ script.js
@@ -20,7 +20,10 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 // Prompt to confirm how many characters the user would like to have in the password
 var lengthNo = prompt("How many characters would you like? Choose between 8 and 128");
-
+if(isNaN(lengthNo)){
+  alert("You must input a number!");
+  return generatePassword()
+}
 if (lengthNo < 8 || lengthNo > 128){
 prompt ("Please choose minimum 8 characters or maximum 128 characters!");
 }
@@ -33,11 +36,13 @@ var confirmSymbols = confirm("Would you like to include special characters?");
 // Loop if answer is outside the parameters 
 if(confirmUpper === false && confirmLower === false && confirmSymbols === false && confirmNumeric === false) {
     alert("You must choose at least one character");
-    var confirmLower = confirm("Would you like to include lowercase leters?");
-    var confirmUpper = confirm("Would you like to include uppercase letters?");
-    var confirmNumeric = confirm("Would you like to include number?");
-    var confirmSymbols = confirm("Would you like to include special characters?");  
+    return generatePassword()
 }
+
+
+
+
+
 };
 
 
