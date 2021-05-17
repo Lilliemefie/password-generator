a30a5e71f13bca49223d68339a1f391af58d414e
lillie <lillie@Lillies-MBP.lan>
2021-05-16 23:09:54 -0700

    added var

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqurstuvwxyz"
-var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ"
-var numeric = 1234567890
-var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+//var generateBtn = document.querySelector("#generate");
 
-console.log(lowerCase);
-console.log(upperCase);
-console.log(numeric);
-console.log(specialCharacter);
+// 1. create pop up for asking about:
+//    - How many character do you want? (from 8 to 128)
+//    - Do you want uppercase?
+//    - Do you want lowercase?
+//    - Do you want number?
+//    - Do you want symbols?
+// 2. 
+//
+/*var randomFunc = {
+  lower = getRandomlowerCase,
+  upper = getRandomUpperCase,
+  symbols = getRandomSymbols,
+  number = getRandomNumeric,
 
+}*/
 
-
-
-// Write password to the #password input
+// Write password to the #password input, created function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -22,5 +26,46 @@ function writePassword() {
 
 }
 
+function getRandomSymbols() {
+  var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function getRandomlowerCase() {
+  var lowerCase = "abcdefghijklmnopqurstuvwxyz";
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+
+function getRandomUpperCase() {
+  var upperCase = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function getRandomNumeric() {
+  var numeric = "0123456789";
+  return numeric[Math.floor(Math.random() * numeric.length)];
+}
+
+
+function asklower(){
+  var yes = confirm ("Do you want a lowercase?");
+  if (yes === true);
+  getRandomlowerCase;
+} else {
+}
+});
+
+
+function askupper(){
+  var yes = confirm ("Do you want an uppercase?");
+  if (yes === true);
+  return getRandomUpperCase;
+}
+
+console.log(asklower, askupper);
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
